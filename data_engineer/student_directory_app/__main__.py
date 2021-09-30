26a096b1166a6b7d0dbf48671b055c69255b5d76
Wilfred Morgan <wil.me.morgan@gmail.com>
2021-09-30 19:43:05 +0800

    Draft create json file feature

--- data_engineer/student_directory_app/__main__.py
+++ data_engineer/student_directory_app/__main__.py
@@ -1,7 +1,10 @@
+from os.path import isfile, getsize
 import pandas as pd
+import json
 
 teachers = pd.read_parquet("teachers.parquet", engine="auto")
 students = pd.read_csv("students.csv", "_", nrows=5)
+
 
 for index, student in students.iterrows():
     teacher = teachers.loc[teachers["cid"] == student["cid"]]
@@ -15,4 +18,13 @@ for index, student in students.iterrows():
         },
     }
 
-    print(student)
+    if not isfile("data.json") or getsize("data.json") == 0:
+        with open("data.json", "w") as f:
+            f.write(json.dumps([student]))
+    else:
+        with open("data.json", "r+") as json_file:
+            data = json.load(json_file)
+
+            data.append(student)
+            with open("data.json", "w") as f:
+                f.write(json.dumps(data))
