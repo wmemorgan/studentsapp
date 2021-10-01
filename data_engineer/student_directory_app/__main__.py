cab4fb00b2972cbf024b26f5e1a9071fb21c01f7
Wilfred Morgan <wil.me.morgan@gmail.com>
2021-10-01 11:10:38 +0800

    Implement large file processing of student data

--- data_engineer/student_directory_app/__main__.py
+++ data_engineer/student_directory_app/__main__.py
@@ -3,15 +3,15 @@ import pandas as pd
 import json
 
 teachers = pd.read_parquet("teachers.parquet", engine="auto")
-students = pd.read_csv("students.csv", "_", nrows=5)
+students = pd.read_csv("students.csv", "_", chunksize=2)
 
 
-for index, student in students.iterrows():
-    teacher = teachers.loc[teachers["cid"] == student["cid"]]
+for student in students:
+    teacher = teachers.loc[teachers["cid"] == student["cid"].values[0]]
     student = {
-        "firstName": student["fname"],
-        "lastName": student["lname"],
-        "classId": student["cid"],
+        "firstName": student["fname"].values[0],
+        "lastName": student["lname"].values[0],
+        "classId": student["cid"].values[0],
         "teacher": {
             "firstName": teacher["fname"].values[0],
             "lastName": teacher["lname"].values[0],
@@ -28,3 +28,27 @@ for index, student in students.iterrows():
             data.append(student)
             with open("data.json", "w") as f:
                 f.write(json.dumps(data))
+
+
+# for index, student in students.iterrows():
+#     teacher = teachers.loc[teachers["cid"] == student["cid"]]
+#     student = {
+#         "firstName": student["fname"],
+#         "lastName": student["lname"],
+#         "classId": student["cid"],
+#         "teacher": {
+#             "firstName": teacher["fname"].values[0],
+#             "lastName": teacher["lname"].values[0],
+#         },
+#     }
+
+#     if not isfile("data.json") or getsize("data.json") == 0:
+#         with open("data.json", "w") as f:
+#             f.write(json.dumps([student]))
+#     else:
+#         with open("data.json", "r+") as json_file:
+#             data = json.load(json_file)
+
+#             data.append(student)
+#             with open("data.json", "w") as f:
+#                 f.write(json.dumps(data))
