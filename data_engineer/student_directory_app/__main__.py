401e596fefd109f8f37f98de0048784ce10d3494
Wilfred Morgan <wil.me.morgan@gmail.com>
2021-10-01 11:13:36 +0800

    Remove unused code

--- data_engineer/student_directory_app/__main__.py
+++ data_engineer/student_directory_app/__main__.py
@@ -28,27 +28,3 @@ for student in students:
             data.append(student)
             with open("data.json", "w") as f:
                 f.write(json.dumps(data))
-
-
-# for index, student in students.iterrows():
-#     teacher = teachers.loc[teachers["cid"] == student["cid"]]
-#     student = {
-#         "firstName": student["fname"],
-#         "lastName": student["lname"],
-#         "classId": student["cid"],
-#         "teacher": {
-#             "firstName": teacher["fname"].values[0],
-#             "lastName": teacher["lname"].values[0],
-#         },
-#     }
-
-#     if not isfile("data.json") or getsize("data.json") == 0:
-#         with open("data.json", "w") as f:
-#             f.write(json.dumps([student]))
-#     else:
-#         with open("data.json", "r+") as json_file:
-#             data = json.load(json_file)
-
-#             data.append(student)
-#             with open("data.json", "w") as f:
-#                 f.write(json.dumps(data))
