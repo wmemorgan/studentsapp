991ae68c11f8566efe7f7f9ca15cd32c307f829d
Wilfred Morgan <wil.me.morgan@gmail.com>
2021-10-02 21:12:07 +0800

    Add test scripts: - test_select_matching_record - test_create_student_record

---
+++ data_engineer/student_directory_app/tests/test_studentsapp.py
@@ -0,0 +1,55 @@
+from io import StringIO
+
+from dask.array.routines import result_type
+from studentsapp import studentsapp
+import pytest
+import dask.dataframe as dd
+import pandas as pd
+
+
+@pytest.fixture(scope="module")
+def student_df():
+
+    return dd.read_csv("data/students.csv", sep="_").head(5)
+
+
+@pytest.fixture(scope="module")
+def teacher_df():
+    df = pd.DataFrame(
+        [
+            ["Jessa", "Gibbs", "08-2046381"],
+            ["Tate", "Weekley", "57-9105495"],
+            ["Trenna", "Chasney", "76-3364242"],
+            ["Rabbi", "McGuinness", "32-3782498"],
+            ["Amalle", "Toffetto", "50-4537323"],
+        ],
+        columns=["fname", "lname", "cid"],
+    )
+
+    return df
+    # return dd.read_table(df)
+
+
+def test_select_matching_record():
+    students_df = dd.read_csv("./data/students.csv", sep="_").head(1)
+    teachers_df = dd.read_parquet("./data/teachers.parquet", ["fname", "lname", "cid"])
+    student = [row for row in students_df.iterrows()][0][1]
+
+    actual = studentsapp.select_matching_record(teachers_df, student, "cid")
+    print("\nactual match:\n", actual)
+    assert actual["fname"].values[0] == "Jessa"
+    assert actual["lname"].values[0] == "Gibbs"
+    assert actual["cid"].values[0] == "08-2046381"
+
+
+def test_create_student_record():
+
+    students_df = dd.read_csv("./data/students.csv", sep="_").head(1)
+    teachers_df = dd.read_parquet("./data/teachers.parquet", ["fname", "lname", "cid"])
+    student = [row for row in students_df.iterrows()][0][1]
+    teacher = teachers_df[teachers_df["cid"] == student["cid"]].compute()
+
+    actual = studentsapp.create_student_record(student, teacher)
+
+    assert actual["firstName"] == "Dniren"
+    assert actual["teacher"]["lastName"] == "Gibbs"
